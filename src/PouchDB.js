26faee62967404064bb8e6789ce25bdda4e50c78
jsbalrog <edwardtjenkins@gmail.com>
2015-03-09 14:12:30 -0600

    Add initial provider and service

---
+++ src/PouchDB.js
@@ -0,0 +1,173 @@
+angular.module('vs.ng-pouch', ['ng']);
+
+angular.module('vs.ng-pouch').provider('pouchConfig', function() {
+  var self = this;
+
+  self.$dwnDbs;
+  self.$upDbs;
+
+  this.$get = function() {
+    return {
+      getDwnDbs: function() {
+        return self.$dwnDbs;
+      },
+      getUpDbs: function() {
+        return self.$upDbs;
+      },
+      getCouchConfig: function() {
+        return self.$couchConfig;
+      }
+    }
+  };
+
+  self.dbs = function(dwnDbs, upDbs) {
+    if(dwnDbs && dwnDbs.length > 0) {
+      self.$dwnDbs = dwnDbs;
+    }
+
+    if(upDbs && upDbs.length > 0) {
+      self.$upDbs = upDbs;
+    }
+  };
+
+  self.couchConfig = function(couchConfig) {
+    if(couchConfig) {
+      self.$couchConfig = couchConfig;
+    }
+  };
+
+});
+
+angular.module('vs.ng-pouch').service('Pouch', function ($q, pouchConfig) {
+  'use strict';
+
+  function getDB(name) {
+    return new PouchDB(name);
+  }
+
+  /**
+   * Function to update the records that are read-only
+   * from the server with whatever changes have been
+   * made on the client but have not yet been synced
+   * to the server, for ui display purposes. This
+   * is called currently from the MasterCtrl, so
+   * basically on app load. change_id is dbname::id;
+   * so, for example, looks something like this:
+   *"gemini_cushion_fist::WO-20150226-2949689"
+   */
+  function shimRecords(downDbName, downDocs) {
+    var deferred = $q.defer();
+    var promises = [];
+    var retVal = [];
+    var jorgeNames = pouchConfig.getUpDbs();
+    // Loop through all of the write databases
+    _.each(jorgeNames, function (jorgeName) {
+      // Get all the documents in the current write db
+      getDB(jorgeName).allDocs({
+        include_docs: true
+      })
+        .then(function (jorgeDocs) {
+          if (jorgeDocs.total_rows > 0) {
+            _.each(downDocs.rows, function(doc) {
+              // The the current write db has any documents, loop through
+              // them and see if any of the documents have a change_id that
+              // matches the current read-only db
+              _.each(jorgeDocs.rows, function (row) {
+                var d = $q.defer();
+                var change_id = row.doc.change_id;
+                var split = change_id.split("::");
+                var dbName = split[0];
+                var id = split[1];
+                if (dbName == downDbName) {
+                  // Find if any document matches
+                  if(doc.id == id) {
+                    _.extend(doc.doc, JSON.parse(row.doc.change));
+                  }
+                }
+                d.resolve(doc);
+                promises.push(d.promise);
+              });
+            });
+          }
+          $q.all(promises).then(function () {
+            deferred.resolve(downDocs);
+          });
+        });
+    });
+
+    return deferred.promise;
+  }
+
+  return function (db) {
+
+    this.db = db;
+
+    this.getAll = function () {
+      var self = this;
+      var deferred = $q.defer();
+      getDB(self.db).allDocs({
+        include_docs: true
+      }).then(function (docs) {
+        return shimRecords(self.db, docs);
+      }).then(function (docs) {
+        deferred.resolve(docs);
+      });
+      return deferred.promise;
+    };
+
+    this.putEntry = function putEntry(table, id, obj, method, endpoint, data, headers, user, successCb, errorCb) {
+      var entry = {
+          _id: new moment().unix().toString(),
+          change_id: '' + table + '::' + id,
+          change: JSON.stringify(obj),
+          method: method,
+          endpoint: endpoint,
+          data: data,
+          headers: headers,
+          user: user
+        },
+        self = this;
+
+      getDB(self.db).put(entry).then(successCb, errorCb);
+    };
+
+    this.syncDB = function syncDB(name, user, opts) {
+      var deferred = $q.defer();
+      var remote = new PouchDB('http://0.0.0.0:5984' + name);
+      //var remote = new PouchDB(couchConfig.host + name);
+      var local = new PouchDB(name);
+      console.log('syncing', name);
+      local.replicate.from(remote, {
+        filter: opts.filter,
+        query_params: opts.getParams(user)
+      }).on('complete', function () {
+        defer.resolve();
+        return deferred.promise;
+      });
+    };
+
+    this.update = function update(db, obj, id) {
+      var deferred = $q.defer();
+      var cushiondb = getDB(db);
+      cushiondb.get(id).then(function success(doc) {
+        var newDoc = _.extend(doc, obj);
+        cushiondb.put(newDoc).then(function success(result) {
+          deferred.resolve(result);
+        }, function error(result) {
+          deferred.reject(result);
+        });
+      }, function error(result) {
+        deferred.reject(result);
+      });
+      return deferred.promise;
+    };
+
+    this.sendJorgeDB = function sendJorgeDB(db, user) {
+      console.log('syncing ' + db + ' for user ' + user.displayName);
+      var deferred = $q.defer();
+      // Do some stuff here.
+      deferred.resolve();
+      return deferred.promise;
+    };
+  };
+});
